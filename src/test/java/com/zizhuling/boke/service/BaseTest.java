fa52a472391ecd76617b3d96e17a9465b0d4c153
hebiao <hebiao@tech-winning.com>
2020-07-23 17:27:41 +0800

    添加单元测试

---
+++ src/test/java/com/zizhuling/boke/service/BaseTest.java
@@ -0,0 +1,22 @@
+package com.zizhuling.boke.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author lishang
+ * @projectName ims
+ * @description: 单元测试基础类，所有的业务测试继承此类
+ * @date 2019/9/16 17:29
+ * @Modified By: wangaogao
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaseTest {
+    @Test
+    public void test() {
+
+    }
+}
